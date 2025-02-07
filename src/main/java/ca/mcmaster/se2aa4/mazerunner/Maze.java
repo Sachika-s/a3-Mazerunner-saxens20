bab7f7045145fb3e95f036c4e37460f7a5955c55
Sachika <saxens20@mcmaster.ca>
2025-02-06 22:36:37 -0500

    Added right hand algo (#11) (#12)

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Maze.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Maze.java
@@ -1,85 +1,97 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.List;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class Maze{
-    private static final Logger logger = LogManager.getLogger();
+    private char[][] maze;
+
+    private int length ;
     private int width;
-    private char[][]mazeArray;
-    private List<char[]> newMaze;
+  
+   
+    private int entry;
+    private int exit;
 
-/**
- * Function - Construcutor of the Maze class
- * Parameter - file given by user  
-**/
-    public Maze(String filePath){
-    
-        mazeArray = readMaze(filePath);
-        width = mazeArray[0].length;
+    private BufferedReader reader;
+    private BufferedReader reader2;
+    private String line;
+
+    public Maze(BufferedReader reader, BufferedReader reader2) {
+        this.reader = reader;
+        this.reader2 = reader2;
     }
 
-/**
- * Function - coverts given file into a 2D array. Uses BufferReader  
- * Parameter - file given by user  
-**/
-    public char[][] readMaze(String filePath){
-        newMaze = new ArrayList<>();
-
+    public char[][] mazetoArray() {
+        maze = new char[length][width];
+        int count = 0;
         try{
-        BufferedReader reader = new BufferedReader(new FileReader(filePath));
-        String row;
-        while((row = reader.readLine()) != null){
-            if(row.trim().length() != 0){
-                newMaze.add(row.toCharArray());
+
+            while ((line = reader2.readLine()) != null) {
+                for (int i = 0; i < width; i++) {
+                    if (i > line.length()-1) {
+                        maze[count][i] = ' ';
+                    }
+                    else {
+                        maze[count][i] = line.charAt(i);
+                    }
+                }
+                count++;
             }
+        } catch(Exception e) {
+            System.out.print("/!\\ An error has occured /!\\");
         }
-        reader.close();
-        return newMaze.toArray(new char[0][]);
-
-    }catch(Exception e){
-        logger.error("An error has occured");
-        return null;
-    }
+        return maze;
     }
 
-/**
- * Function - boolean check to verfiy is path is free to move
- * Parameter - coordinates from array  
-**/
+    public void sizeArray() {
+        try{
+            while ((line = reader.readLine()) != null) {
+                length++;
+                for (int i = 0; i < line.length(); i++) {
+                    width = line.length();
+                }
+            }
 
-    public boolean emptyspaceCheck(int x, int y ){
-        boolean check = mazeArray[y][x] == ' ';
-        return check; 
-    }
-/**
- * Function - boolean check to verfiy for WALL
- * Parameter - coordinates from array  
-**/
-    public boolean wallCheck(int x, int y ){
-        boolean check = mazeArray[y][x] == '#';
-        return check; 
-    }
-/**
- * Function - getter method for Maze Array
- * Parameter - no parameters needed   
-**/
-
-    public char[][] getMazeArray() {
-        return mazeArray;
+        } catch(Exception e) {
+            System.out.print("/!\\ An error has occured /!\\");
+        }
     }
 
- /**
- * Function - getter for width of array
- * Parameter - no parameters   
-**/
+
+
+    
+   
     public int getWidth() {
         return width;
     }
 
+   
+    public int getLength() {
+        return length;
+    }
+
+   
+    public int getEntry() {
+        for (int i = 0; i < length; i++) {
+            if (maze[i][0] == ' ') {
+                entry = i;
+            }
+        }
+        return entry;
+    }
+
+  
+    public int getExit() {
+        for (int j = 0; j < length; j++) {
+            if (maze[j][width-1] == ' ') {
+                exit = j;
+            }
+        }
+        return exit;
+    }
 
 }
+
+
--- src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeSolver.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeSolver.java
@@ -1,47 +1,123 @@
 package ca.mcmaster.se2aa4.mazerunner;
-public class MazeSolver{
-    private Compass compass;
 
-/**
- * Function - constructor of mazeSolver class 
- * Parameter - none needed  
-**/
-    public MazeSolver() {
-       this.compass = new Compass();
+
+public class MazeSolver {
+    protected Compass.Direction direction;
+    private Compass travel;
+    private RightHandAlgo algo;
+
+    private char[][] maze;
+    private int width, length;
+    private int r, c;
+    private int entry, exit;  
+    private int[] pos;
+
+    public MazeSolver(char[][] maze, int width, int length, int entry, int exit, String direction) {
+        this.maze = maze;
+        this.direction = Compass.Direction.valueOf(direction.toUpperCase());
+        this.algo = new RightHandAlgo(maze);
+        this.entry = entry;
+        this.exit = exit;
+        this.pos = new int[]{entry, 0};
+        this.width = width;
+        this.length = length;
     }
-/**
- * Function - identfies starting point 
- * Parameter - array maze 
-**/
-    public int entryPoint(char[][] maze){
-        for(int i = 0; i < maze[0].length; i++){
-            if(maze[0][i] == ' '){
-                return i;
+
+    public String findPath() {
+        StringBuffer path = new StringBuffer();
+        travel = new Compass(direction, maze, width, length);
+        travel.Direction();
+        direction = travel.getCompass();
+        r = travel.getRow();
+        c = travel.getCol();
+
+        while (pos[0] != exit || pos[1] != width - 1) {
+            int newR = pos[0] + r;
+            int newC = pos[1] + c;
+
+            if (newR >= 0 && newR < length && newC >= 0 && newC < width) {
+                String move = algo.Play(direction, pos[0], pos[1], r, c);
+                travel.currentDirection(move);
+                travel.Direction();
+                direction = travel.getCompass();
+                r = travel.getRow();
+                c = travel.getCol();
+                path.append(move);
+                if (move.contains("F")) {
+                    pos[0] += r;
+                    pos[1] += c;
+                }
             }
         }
-        return 0;
+        System.out.println("Computed Path: " + path.toString());
+        return path.toString();
     }
-/**
- * Function - implements a simple algorithm to move through straight maze
- * Parameter - maze object from Maze class 
-**/
-    public String findPath(Maze maze){
-        
-        char[][] mazeArray = maze.getMazeArray();
-        int entry = entryPoint(mazeArray);
-        int y = 0;
-        
 
-        for ( int x = entry ; x < maze.getWidth(); x++){
-            if(maze.wallCheck(x,y) == true){
-                compass.move(Direction.FORWARD);
-            }else{
-                break;
+
+    public static String factorToCan(String path) {
+        StringBuilder expandedP = new StringBuilder();
+        int i = 0;
+        StringBuilder repeat = new StringBuilder();
+    
+        while (i < path.length()) {
+            char current = path.charAt(i);
+    
+            if (Character.isDigit(current)) {
+                repeat.append(current);
+            } else if (current == ' ') {
+                i++; 
+                continue;
+            } else {
+                int Counter = repeat.length() > 0 ? Integer.parseInt(repeat.toString()) : 1;
+                repeat.setLength(0); 
+    
+                for (int j = 0; j < Counter; j++) {
+                    expandedP.append(current);
+                }
             }
+            i++;
         }
-        return compass.getPath();
-        }
+    
+        return expandedP.toString();
+    }
+    
+
+    public static String canToFactor(String path) {
 
 
+        StringBuilder factorized = new StringBuilder();
+        char prev = path.charAt(0);
+        int count = 1;
+
+        for (int i = 1; i < path.length(); i++) {
+            char curr = path.charAt(i);
+            if (curr == prev) {
+                count++;
+            }   else {
+                if (count > 1) {
+                    factorized.append(count);
+             }
+             factorized.append(prev);
+                prev = curr;
+                count = 1;
+          }
+    }
+
+        if (count > 1) {
+            factorized.append(count);
+        }
+        factorized.append(prev);
+
+        return factorized.toString();
+}
+
+public String FactorizedPath() {
+    return canToFactor(findPath()); 
+}
+
+public String CanonicalPath() {
+    return factorToCan(findPath()); 
+}
 
 }
+
